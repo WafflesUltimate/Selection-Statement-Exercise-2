e20b08e773161ab1eb9be779aa6eaabc06d8242c
David Armstrong <wafflesultimate1@gmail.com>
2023-10-03 05:51:19 -0700

    Completed exercises one and two

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,42 @@
 {
     public class Program
     {
+        static void FavSubject()
+        {
+            Console.WriteLine("What is you Favorite Subject?");
+            string favSubject = Console.ReadLine();
+
+            switch(favSubject)
+            {
+                case string x when favSubject.StartsWith("hi"):
+                    Console.WriteLine("History is the best.");
+                    break;
+
+                case string x when favSubject.StartsWith("mu"):
+                    Console.WriteLine("Music is nice.");
+                    break;
+
+                case string x when favSubject.StartsWith("eng"):
+                    Console.WriteLine("I fell asleep in English once.");
+                    break;
+
+                case string x when favSubject.StartsWith("spa"):
+                    Console.WriteLine("Spanish is fun.");
+                    break;
+
+                case string x when favSubject.StartsWith("ma"):
+                    Console.WriteLine("Math is boring.");
+                    break;
+
+                default:
+                    Console.WriteLine("Sorry, I don't recognise that subject.");
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
         }
     }
 }
